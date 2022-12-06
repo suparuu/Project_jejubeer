adb920a98c30eceba5a2691ae01d19c9b842e6d4
suparuu <kkwoo97@naver.com>
2022-12-06 15:49:33 +0900

    01232

---
+++ js/index.js
@@ -0,0 +1,71 @@
+/* const Ani01 = document.querySelector('.subtitle01'),
+Ani02 = document.querySelector('.subtitle02'),
+        Ani03 = document.querySelector('.subtitle03'),
+        Ani04 = document.querySelector('.subtitle04'); */
+        
+        
+        /*     let A = Ani01.offsetHeight,
+        B = Ani02.offsetHeight,
+        C = Ani03.offsetHeight,
+        D = Ani04.offsetHeight;
+        */
+       
+       /*
+       console.log(A,B,C,D) */
+    
+        const Ani01 = document.querySelector('.subtitle01'),
+        Ani02 = document.querySelector('.subtitle02'),
+        Ani03 = document.querySelector('.subtitle03'),
+        Ani04 = document.querySelector('.subtitle04');
+        
+    
+function aa() {
+
+    let A = window.pageYOffset,
+        B = Ani01.offsetHeight,
+        C = Ani02.offsetHeight,
+        D = Ani03.offsetHeight,
+        E = Ani04.offsetHeight;
+     
+    console.log(A,B,C,Ani01,Ani02,Ani03,Ani04,)
+/*     if (B <= A) {
+        Ani01.classList.add('on')
+        
+    }
+    if (B + C <= A) {
+        Ani01.classList.remove('on')
+        Ani02.classList.add('on')
+    }
+    if (B + C + D <= A) {
+        Ani02.classList.remove('on')
+        Ani03.classList.add('on')
+    }
+    if (B + C + D + E <= A) {
+        Ani03.classList.remove('on')
+        Ani04.classList.add('on')
+    } */
+    if (B <= A) {
+        Ani01.classList.add('on')
+        
+    }
+    if (B + C <= A) {
+        Ani01.classList.remove('on')
+        Ani02.classList.add('on')
+    }
+    if (B + C + D <= A) {
+        Ani02.classList.remove('on')
+        Ani03.classList.add('on')
+    }
+    if (B + C + D + E <= A) {
+        Ani03.classList.remove('on')
+        Ani04.classList.add('on')
+    }
+    
+    
+
+}
+window.addEventListener('scroll', aa)
+    
+    
+    
+
