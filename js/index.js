82c5b5ab344fffae4799946de4cc036598583039
Eomtae <118143333+Eomtae@users.noreply.github.com>
2022-12-12 11:12:04 +0900

    2

--- js/index.js
+++ js/index.js
@@ -1,71 +1,35 @@
-/* const Ani01 = document.querySelector('.subtitle01'),
-Ani02 = document.querySelector('.subtitle02'),
-        Ani03 = document.querySelector('.subtitle03'),
-        Ani04 = document.querySelector('.subtitle04'); */
-        
-        
-        /*     let A = Ani01.offsetHeight,
-        B = Ani02.offsetHeight,
-        C = Ani03.offsetHeight,
-        D = Ani04.offsetHeight;
-        */
-       
-       /*
-       console.log(A,B,C,D) */
-    
-        const Ani01 = document.querySelector('.subtitle01'),
-        Ani02 = document.querySelector('.subtitle02'),
-        Ani03 = document.querySelector('.subtitle03'),
-        Ani04 = document.querySelector('.subtitle04');
-        
-    
+$('body').prepend('<header>');
+$('body').append('<footer>');
+
+$('header').load('./inc.html header>section');
+$('footer').load('./inc.html footer>div');
+
+
+const Ani01 = document.querySelector(".subtitle01"),
+  Ani02 = document.querySelector(".subtitle02"),
+  Ani03 = document.querySelector(".subtitle03"),
+  Ani04 = document.querySelector(".subtitle04");
+
 function aa() {
+  let A = window.pageYOffset,
+    B = Ani01.scrollHeight,
+    C = Ani02.scrollHeight,
+    D = Ani03.scrollHeight,
+    E = Ani04.scrollHeight;
 
-    let A = window.pageYOffset,
-        B = Ani01.offsetHeight,
-        C = Ani02.offsetHeight,
-        D = Ani03.offsetHeight,
-        E = Ani04.offsetHeight;
-     
-    console.log(A,B,C,Ani01,Ani02,Ani03,Ani04,)
-/*     if (B <= A) {
-        Ani01.classList.add('on')
-        
-    }
-    if (B + C <= A) {
-        Ani01.classList.remove('on')
-        Ani02.classList.add('on')
-    }
-    if (B + C + D <= A) {
-        Ani02.classList.remove('on')
-        Ani03.classList.add('on')
-    }
-    if (B + C + D + E <= A) {
-        Ani03.classList.remove('on')
-        Ani04.classList.add('on')
-    } */
-    if (B <= A) {
-        Ani01.classList.add('on')
-        
-    }
-    if (B + C <= A) {
-        Ani01.classList.remove('on')
-        Ani02.classList.add('on')
-    }
-    if (B + C + D <= A) {
-        Ani02.classList.remove('on')
-        Ani03.classList.add('on')
-    }
-    if (B + C + D + E <= A) {
-        Ani03.classList.remove('on')
-        Ani04.classList.add('on')
-    }
-    
-    
+  if (A >= B) {
+    Ani01.classList.add("on");
+  }
+  if (A >= (B + C)) {
+    Ani02.classList.add("on");
+  }
+  if (A >= (B + C + D)) {
+    Ani03.classList.add("on");
+  }
+  if (A >= (B + C + D + E)) {
+    Ani04.classList.add("on");
+  }
 
+  console.log(A, B, C, D, E);
 }
-window.addEventListener('scroll', aa)
-    
-    
-    
-
+window.addEventListener("scroll", aa);
